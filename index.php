954fa14a2967f93247f04077f05a8b4aba915a9e
Alok Kumar <alok760@gmail.com>
2017-07-25 00:32:54 +0530

    fixed mobile view

--- index.php
+++ index.php
@@ -1,28 +1,7 @@
-<?php
 
-$data = get_csv_content('https://docs.google.com/spreadsheets/d/1GFG-phtItT3V3JGGqyZgpN6aViDkY-rg3HUTBeOjD3Q/pub?output=csv');
-
-	function get_csv_content($spreadsheet_url){
-		if(!ini_set('default_socket_timeout', 15)) 
-		echo "<!-- unable to change socket timeout -->";
-
-		if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
-			while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-				$spreadsheet_data[] = $data;
-			}
-			fclose($handle);
-			return $spreadsheet_data;
-		}
-	}
-	
-	function validate_data($data){
-		return true;
-	}
-    
-?>
 	
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -30,6 +9,28 @@ $data = get_csv_content('https://docs.google.com/spreadsheets/d/1GFG-phtItT3V3JG
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <link href="css/style.css" rel="stylesheet" type="text/css" />
     </head>
+    <?php
+
+    $data = get_csv_content('https://docs.google.com/spreadsheets/d/1GFG-phtItT3V3JGGqyZgpN6aViDkY-rg3HUTBeOjD3Q/pub?output=csv');
+
+        function get_csv_content($spreadsheet_url){
+            if(!ini_set('default_socket_timeout', 15)) 
+            echo "<!-- unable to change socket timeout -->";
+
+            if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
+                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                    $spreadsheet_data[] = $data;
+                }
+                fclose($handle);
+                return $spreadsheet_data;
+            }
+        }
+
+        function validate_data($data){
+            return true;
+        }
+
+    ?>
     <body>
         <div class="container-fluid">
             <div class="row">
@@ -46,7 +47,6 @@ $data = get_csv_content('https://docs.google.com/spreadsheets/d/1GFG-phtItT3V3JG
             </div>
 
             <div class="row">
-                <div class="col-xs-7">
                    <img src="map2.gif" usemap="#image-map" id="map" > 
 
                         <map name="image-map">
@@ -91,14 +91,14 @@ $data = get_csv_content('https://docs.google.com/spreadsheets/d/1GFG-phtItT3V3JG
                         </map>
                                             
 
-                </div>               
-                    <div class="col-xs-4" id="data">
-                        <center> <h3>   NRC details : </h3> </center>
-                               
-                        <p id="para">
-                                <br><br><br><br><br> &emsp; &emsp; Select a District on the MAP <br><br><br><br><br><br><br>
-                        </p>
+                </div>  
+                
+                            <center> <h3>   NRC details : </h3> </center>
 
+                            <p id="para" style="padding-left:5px;">
+                                    <br><br><br><br><br> &emsp; &emsp; Select a District on the MAP <br><br><br><br><br><br><br>
+                            </p>
+                        </div>
                     </div>           	
             </div>
         </div>
